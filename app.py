6a9a7aa0d10f4a348cb189df6a4cb21f89c4f139
mohana12098 <mohana12098@gmail.com>
2026-06-18 08:07:15 +0530

    get api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import psycopg2
 
 app =Flask(__name__)
@@ -34,6 +34,68 @@ def create_student_table():
     connection.close()
 
 create_student_table()
+
+@app.route("/send_data", methods = {'POST'})
+def send_data():
+    student_name = request.json['student_name']
+    roll_number = request.json['roll_number']
+    email = request.json['email']
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("""
+        INSERT INTO student_table(student_name,roll_number, email) VALUES(%s,%s,%s)
+""",(student_name, roll_number, email))
+    connection.commit()
+    cur.close()
+    connection.close()
+    return jsonify({"message":"data sended successfully"}),201
+
+@app.route("/get_data", methods =['GET'])
+def get_data():
+    connection =get_db_connection()
+    cur =connection.cursor()
+    cur.execute("""
+        select * from student_table
+""")
+    Data = cur.fetchone()
+    cur.close()
+    connection.close()
+    return jsonify({
+        "student_id":Data[0],
+        "student_name":Data[1],
+        "roll_number":Data[2],
+        "email":Data[3]
+    }),200
+
+@app.route("/put_data", methods =['PUT'])
+def put_data():
+    #student_id=request.json['student_id']
+    #student_name = request.json['student_name']
+    #roll_number = request.json['roll_number']
+    email = request.json['email']
+    connection =get_db_connection()
+    cur =connection.cursor()
+    cur.execute("""
+        UPDATE student_table SET email='srinu@email.com' WHERE student_id= 12
+""")
+    connection.commit()
+    cur.close()
+    connection.close()
+    return jsonify({"message":"data updated successfully"}),202
+
+@app.route("/delete_data", methods =['DELETE'])
+def delete_data():
+    student_id=request.json['student_id']
+    connection =get_db_connection()
+    cur =connection.cursor()
+    cur.execute("""
+        DELETE FROM student_table WHERE student_id=%s
+""",(student_id,))
+    connection.commit()
+    cur.close()
+    connection.close()
+    return jsonify({"message":"data delete successfully"}),203
+
 
 if __name__== "__main__":
     app.run(debug=True)
