ae4cdd2ec887be2ab30a4bb4d5c22c0c12621728
mohana12098 <mohana12098@gmail.com>
2026-06-25 15:28:12 +0530

    done

--- app.py
+++ app.py
@@ -43,6 +43,9 @@ def signup():
     username =request.json['username']
     email =request.json['email']
     password =request.json['password']
+
+    if not username or email or password:
+        return jsonify({"error":"all fields required"}),400
     hashed_password =bcrypt.generate_password_hash(password).decode("utf-8")
     connection =get_db_connection()
     cur = connection.cursor()
@@ -53,6 +56,35 @@ def signup():
     cur.close()
     connection.close()
     return jsonify({"message":"signup successfully"})
+
+@app.route('/login',methods=['POST'])
+def login():
+    email = request.json['email']
+    password =request.json['password']
+    if not email or not password:
+        return jsonify(({"error":"all fields are required"})),400
+    connection =get_db_connection()
+    cur =connection.cursor()
+    cur.execute("""
+          select user_id, username, password from users_table
+                where email =%s
+""",(email,))
+    user =cur.fetchone()
+    cur.close()
+    connection.close()
+    if not user:
+        return jsonify({"error":"user not found"})
+    user_id, username, hashed_password=user
+    if not bcrypt.check_password_hash(hashed_password,password):
+        return jsonify({"error":"invalid password"}),401
+    return jsonify({
+        "message":"login successful",
+        "user":{
+            "user_id":user_id,
+            "username":username,
+            "email":email
+        }
+    })
 
 if __name__== "__main__":
     app.run(debug=True)
